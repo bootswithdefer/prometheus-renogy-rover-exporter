92756d850d2e71762182635fd5dc5daa674b0cae
defer <defer@dotd.lan>
2022-07-18 19:42:56 +0000

    add temps, remove redundant power metric

--- prometheus-rover-exporter.py
+++ prometheus-rover-exporter.py
@@ -4,10 +4,22 @@ from prometheus_client import start_http_server, Gauge
 import time
 
 g_state_of_charge = Gauge("renogy_rover_state_of_charge", "Percent state of charge")
-g_battery_voltage = Gauge("renogy_rover_battery_voltage", "")
+g_battery_voltage = Gauge("renogy_rover_battery_voltage", "Battery voltage")
+
+g_controller_temperature_c = Gauge(
+    "renogy_rover_controller_temperature_c", "Controller temperature in Celcius"
+)
+g_battery_temperature_c = Gauge(
+    "renogy_rover_battery_temperature_c", "Battery temperature in Celcius"
+)
+g_controller_temperature_f = Gauge(
+    "renogy_rover_controller_temperature_f", "Controller temperature in Fahrenheit"
+)
+g_battery_temperature_f = Gauge(
+    "renogy_rover_battery_temperature_f", "Battery temperature in Fahrenheit"
+)
 
 g_solar_voltage = Gauge("renogy_rover_solar_voltage", "Solar volage")
-g_solar_power = Gauge("renogy_rover_solar_power", "Solar power in watts")
 g_solar_current = Gauge("renogy_rover_solar_current", "Solar current in amps")
 
 g_charge_power = Gauge("renogy_rover_charge_power", "Charge power in watts")
@@ -33,9 +45,17 @@ def get_rover_metrics():
     BatVolt = client.read_holding_registers(0x101, 1, unit=1)
     g_battery_voltage.set(float(BatVolt.registers[0] * 0.1))
 
-    # Temps = client.read_holding_registers(0x103, 2, unit=1)
-    # controllerTemp = Temps.registers[0]
-    # batteryTemp = Temps.registers[1]
+    ChargeCurrent = client.read_holding_registers(0x102, 1, unit=1)
+    g_charge_current.set(float(ChargeCurrent.registers[0] * 0.01))
+
+    Temps = client.read_holding_registers(0x103, 2, unit=1)
+    temp_c = (Temps.registers[0] >> 0) & 0xFF
+    g_battery_temperature_c.set(temp_c)
+    g_battery_temperature_f.set(temp_c * 1.8 + 32)
+
+    temp_c = (Temps.registers[0] >> 8) & 0xFF
+    g_controller_temperature_c.set(temp_c)
+    g_controller_temperature_f.set(temp_c * 1.8 + 32)
 
     PanelVolt = client.read_holding_registers(0x107, 1, unit=1)
     g_solar_voltage.set(float(PanelVolt.registers[0] * 0.1))
@@ -46,14 +66,10 @@ def get_rover_metrics():
     ChargeWatts = client.read_holding_registers(0x109, 1, unit=1)
     g_charge_power.set(ChargeWatts.registers[0])
 
-    ChargeCurrent = client.read_holding_registers(0x102, 1, unit=1)
-    g_charge_current.set(float(ChargeCurrent.registers[0] * 0.01))
-
-    PanelWatts = client.read_holding_registers(0x109, 1, unit=1)
-    g_solar_power.set(PanelWatts.registers[0])
-
     ChargingState = client.read_holding_registers(0x120, 1, unit=1)
     g_charging_state.set(ChargingState.registers[0])
+
+    client.close()
 
 
 if __name__ == "__main__":
